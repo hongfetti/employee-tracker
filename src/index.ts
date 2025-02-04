34dee09f66c95e5baaa0f404744dd82697375605
hongfetti <ryan.hong1994@gmail.com>
2025-02-04 15:12:29 -0500

    able to pull up CLI now-application not complete but can now test easily as i go-saving this

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,73 @@
-// import inquirer from 'inquirer';
+import inquirer from 'inquirer';
+import { pool } from './db/connection.js';
+
+class Cli {
+    exit: boolean = false;
+
+    startApp() {
+    inquirer
+        .prompt([{
+            type: "list",
+            name: "selectActions",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "Add Employee",
+                "Update Employee Role",
+                "View All Roles",
+                "Add Role",
+                "View All Departments",
+                "Add Department",
+                "Quit",
+            ],
+        }
+    ])
+        .then( async (answers)  => {
+            if (answers.action === "View All Emloyees") {
+                try {
+                    const query = `
+                    SELECT 
+                        e1.id AS employee_id, 
+                        e1.first_name AS employee_name, 
+                        e1.last_name, 
+                        e2.first_name AS manager_name,
+                        department.department_name,
+                        role.title 
+                    FROM employee e1
+                    LEFT JOIN employee e2 ON e1.manager_id = e2.id 
+                    JOIN role ON e1.role_id = role.id
+                    JOIN department ON role.department_id = department.id;
+                    `                   
+                    const result = await pool.query(query);
+                    console.table(result.rows)
+                } catch (err) {
+                    console.error('Error connecting to database:', err);
+                    this.startApp()
+                }
+            }
+        // } else if (answers.action === 'Add Employee') {
+
+        // } else if (answers.action === 'Update Employee Role') {
+
+        // } else if (answers.action === 'View All Roles') {
+
+        // } else if (answers.action === 'Add Role') {
+
+        // } else if (answers.action === 'View All Departments') {
+
+        // } else if (answers.action === 'Add Department') {
+
+        // }
+         else {
+        this.exit = true;
+    }
+    if (!this.exit) {
+        this.startApp();
+    }
+    });
+}
+}
+
+
+const test = new Cli
+test.startApp()
